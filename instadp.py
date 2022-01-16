2f7e85ab7d39e4938bb54c32229808b8b9ac3302
Arvinder Singh Dhoul <asdhoul004@gmail.com>
2022-01-16 21:16:46 +0530

    Delete instadp.py

--- instadp.py
+++
@@ -1,8 +0,0 @@
-#program to download instagram profile picture of any account only by typing its username
-
-
-import instaloader          #library
-
-ig = instaloader.Instaloader()
-dp = input("ENTER INSTAGRAM USERNAME::")          #username
-ig.download_profile(dp.profile_pic_only == True)
